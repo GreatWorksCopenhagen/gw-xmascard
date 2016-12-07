ec8b3b057ddb6868b42dd721581b8bf2fb30a8d3
GreatWorks Copenhagen / Frederic Dile <fd@greatworks.dk>
2016-12-07 13:54:19 +0100

    Update data js file with tracks

--- src/data/data.js
+++ src/data/data.js
@@ -6,55 +6,440 @@ export default class Data {
               names: 'Louise og Rasmus',
               playlistTitle: 'Have a Danish top-pop X-mas',
               playlist: 'PLq_oRfIJoxLek4Ul4SFMXx8hPmDvbeQTk',
-              cover:'/assets/images/covers/louiserasmus.jpg'
+              cover:'/assets/images/covers/louiserasmus.jpg',
+              tracks:[
+                  {
+                  	title:"Vi Ønsker Jer Alle En Glædelig Jul ",
+                  	artist:"Tommy Seebach & Annette Heick "
+                  },
+                  {
+                  	title:"Søren Banjomus ",
+                  	artist:" Otto Brandenburg & Spillopperne "
+                  },
+                  {
+                  	title:"Boom Boom ",
+                  	artist:"Mabel"
+                  },
+                  {
+                  	title:"Do You Speak English ",
+                  	artist:" Keld Heick & Hilda Heick "
+                  },
+                  {
+                  	title:"Bjældeklang / Kender I Den Om Rudolf ",
+                  	artist:" John Mogensen "
+                  },
+                  {
+                  	title:"Vimmersvej",
+                  	artist:" Bamses Venner "
+                  },
+                  {
+                  	title:"Der Er Noget I Luften ",
+                  	artist:"Kim Larsen "
+                  },
+                  {
+                  	title:"Rap Jul ",
+                  	artist:"Shu-Bi-Dua "
+                  },
+                  {
+                  	title:"From All of Us to All of You ",
+                  	artist:" Sweethearts"
+                  },
+                  {
+                  	title:"Jul Det’ Cool ",
+                  	artist:"Mc Einar "
+                  }
+              ]
           },
           {
               names: 'Pia, Christian and Christian',
               playlistTitle: 'Merry Christmas from The Moms And Dads',
               playlist: 'PLq_oRfIJoxLfCFniKFJL3RRsQVwsC4IXY',
-              cover:'/assets/images/covers/partners.jpg'
+              cover:'/assets/images/covers/partners.jpg',
+              tracks:[
+                {
+                	title:"I saw Mommy Kissing Santa Claus",
+                	artist:"The Jackson 5"
+                },
+                {
+                	title:"Please Daddy  Don't Get Drunk This Christmas",
+                	artist:"John Denver"
+                },
+                {
+                	title:"My Mom and Santa Claus ",
+                	artist:"George Jones"
+                },
+                {
+                	title:"Santa Looked a Lot Like Daddy",
+                	artist:"Buck Owens"
+                },
+                {
+                	title:"Merry Christmas Baby",
+                	artist:"Ike and Tina Turner"
+                },
+                {
+                	title:"I Believe in Father Christmas",
+                	artist:"Greg Lake"
+                },
+                {
+                	title:"Daddy's Drinking Up Our Christmas",
+                	artist:"Commander Cody"
+                },
+                {
+                	title:"Mary’s Boy Child",
+                	artist:"Harry Belafonte"
+                },
+                {
+                	title:"Father Christmas",
+                	artist:"The Kinks"
+                },
+                {
+                	title:"Merry Christmas from the Family",
+                	artist:"Robert Earl Keen"
+                },
+                {
+                	title:"There’s no place like home for the holidays",
+                	artist:"Bing Crosby"
+                },
+                {
+                	title:"Christmas Baby Please Come Home",
+                	artist:"Darlene Love"
+                },
+                {
+                	title:"Back Door Santa",
+                	artist:"Clarence Carter"
+                },
+                {
+                	title:"Little Drummer Boy",
+                	artist:"David Bowie and Bing Crosby"
+                },
+                {
+                	title:"Grandma was run over by Santa",
+                	artist:"Elmo and Patsy"
+                },
+                {
+                	title:"Santa Claus got stuck in my chimney",
+                	artist:"Ella Fitzgerald"
+                }
+              ]
           },
-          {
-              names: 'Josefine og Esben',
-              playlistTitle: 'Josefine og Esbens playlist',
-              playlist: 'PLq_oRfIJoxLfCFniKFJL3RRsQVwsC4IXY',
-              cover:'/assets/images/covers/joseesben.jpg'
-          },
+          // {
+          //     names: 'Josefine og Esben',
+          //     playlistTitle: 'Josefine og Esbens playlist',
+          //     playlist: 'PLq_oRfIJoxLfCFniKFJL3RRsQVwsC4IXY',
+          //     cover:'/assets/images/covers/joseesben.jpg',
+          //     tracks:[
+          //       {
+          //
+          //       }
+          //     ]
+          // },
           {
               names: 'Bo',
               playlistTitle: 'Bos playlist',
               playlist: 'PLq_oRfIJoxLfCFniKFJL3RRsQVwsC4IXY',
-              cover:'/assets/images/covers/bo.jpg'
+              cover:'/assets/images/covers/bo.jpg',
+              tracks:[
+                {
+                	title:"Sleigh Ride",
+                	artist:"Boney James"
+                },
+                {
+                	title:"This Christmas",
+                	artist:"Donny Hathaway"
+                },
+                {
+                	title:"Christmas Gift",
+                	artist:"Margie Joseph"
+                },
+                {
+                	title:"I’ll Be Your Santa Baby",
+                	artist:"Rufus Thomas"
+                },
+                {
+                	title:"Ain't No Chimneys In The Projects",
+                	artist:"Sharon Jones & the Dap-Kings"
+                },
+                {
+                	title:"Santa's Got A Bag Of Soul",
+                	artist:"The Soul Saints Orchestra"
+                },
+                {
+                	title:"Spending Christmas",
+                	artist:"Chuck Berry"
+                },
+                {
+                	title:"Santa Claus Go Straight To The Ghetto",
+                	artist:"James Brown"
+                },
+                {
+                	title:"Back Door Santa",
+                	artist:"Clarence Carter"
+                },
+                {
+                	title:"Gee Whiz its Christmas",
+                	artist:"Carla Thomas"
+                }
+              ]
           }
           {
               names: 'Frederic',
               playlistTitle: 'Tijuana Xmas',
               playlist: 'PLq_oRfIJoxLdTLNA75vHQT9THdKd2bvO9',
-              cover:'/assets/images/covers/fred.jpg'
+              cover:'/assets/images/covers/fred.jpg',
+              tracks:[
+                {
+                	title:"Edition spéciale Russie",
+                	artist:"what the cut"
+                },
+                {
+                	title:"Le clip chinois le plus WTF ! ",
+                	artist:"Prydaz"
+                },
+                {
+                	title:"Go",
+                	artist:"Valley Lodge "
+                },
+                {
+                	title:"Mr. Trololo",
+                	artist:"Mr. Trololo"
+                },
+                {
+                	title:"Mr Garrison Merry F ing Christmas",
+                	artist:"South Park"
+                },
+                {
+                	title:"Mr.Hanky The Christmas Poo",
+                	artist:"South Park"
+                },
+                {
+                	title:"Austin Powers Theme ",
+                	artist:"George Clinton"
+                },
+                {
+                	title:"Dernier Domicile Connu ",
+                	artist:"François de Roubaix"
+                },
+                {
+                	title:"La Scoumoune ",
+                	artist:"François de Roubaix"
+                },
+                {
+                	title:"Les Dunes D'ostende",
+                	artist:"François de Roubaix"
+                }
+              ]
           },
           {
               names: 'Tarik',
               playlistTitle: 'Weird Xmas',
               playlist: 'PLq_oRfIJoxLdLUAGv0OfVukxLgBM3AbKj',
-              cover:'/assets/images/covers/tarik.jpg'
+              cover:'/assets/images/covers/tarik.jpg',
+              tracks:[
+                {
+                	title:"Scary F**ked Up Christmas",
+                	artist:"Garfunkel and Oates (Featuring Doug Benson) "
+                },
+                {
+                	title:"Merry Christmas (I Don't Want To Fight Tonight) ",
+                	artist:"The Ramones"
+                },
+                {
+                	title:"Santa Claus The Original Hippie ",
+                	artist:"Homer & Jethro"
+                },
+                {
+                	title:"Dear Santa (Bring Me a Man This Christmas)",
+                	artist:"The Weather Girls"
+                },
+                {
+                	title:"To Heck With Ole Santa Claus ",
+                	artist:"Loretta Lynn"
+                },
+                {
+                	title:"Santa Dog",
+                	artist:"THE RESIDENTS"
+                },
+                {
+                	title:"Christmas At Ground Zero ",
+                	artist:"Weird Al Yankovic"
+                },
+                {
+                	title:"Noel Christmas ",
+                	artist:"(CD: Disco Party Medley) "
+                },
+                {
+                	title:"Maria Donna",
+                	artist:"Now that it's Christmas "
+                }
+              ]
           },
           {
               names: 'Jonas',
               playlistTitle: 'All I want for Christmas',
               playlist: 'PLq_oRfIJoxLe1dUku-fu3PL3uqwoXdGw0',
-              cover:'/assets/images/covers/jonas.jpg'
+              cover:'/assets/images/covers/jonas.jpg',
+              tracks:[
+                {
+                	title:"Christmas Is",
+                	artist:"RUN D.M.C"
+                },
+                {
+                	title:"Santa Baby",
+                	artist:"Eartha Kitt "
+                },
+                {
+                	title:"Christmas Rappin'",
+                	artist:"Kurtis Blow "
+                },
+                {
+                	title:"Merry Mutafuckin' Xmas",
+                	artist:"Eazy E"
+                },
+                {
+                	title:"Santa Claus Go Straight To The Ghetto ",
+                	artist:"James Brown "
+                },
+                {
+                	title:"Back Door Santa",
+                	artist:"Clarence Carter"
+                },
+                {
+                	title:"Santa Rap",
+                	artist:"The Treacherous Three"
+                },
+                {
+                	title:"This Christmas",
+                	artist:"Donny Hathaway "
+                },
+                {
+                	title:"Santa Clause is Coming To Town",
+                	artist:"Jackson 5 "
+                },
+                {
+                	title:"COLD CHILLIN' CHRISTMAS",
+                	artist:"JUICE CREW ALL-STARS"
+                }
+              ]
           },
           {
               names: 'Dani',
               playlistTitle: 'Dani’s Christmas Cooking Playlist',
               playlist: 'PLq_oRfIJoxLeuwB6nIyQTKun1_65PibL8',
-              cover:'/assets/images/covers/dani.jpg'
+              cover:'/assets/images/covers/dani.jpg',
+              tracks:[
+                {
+                	title:"A marchmallow world",
+                	artist:"Dean Martin and Frank Sinatra"
+                },
+                {
+                	title:"Christmas in Hollis",
+                	artist:"RUN-DMC"
+                },
+                {
+                	title:"Chestnuts roasting on an open fire ",
+                	artist:"Nat King Cole"
+                },
+                {
+                	title:"Sleigh Ride ",
+                	artist:"RONETTES"
+                },
+                {
+                	title:"Grandma Got Run over by a Reindeer ",
+                	artist:"Elmo & Patsy"
+                },
+                {
+                	title:"CoolYule ",
+                	artist:"Louis Armstrong"
+                },
+                {
+                	title:"Let it Snow! ",
+                	artist:"Dean Martin"
+                },
+                {
+                	title:"Chinese Food On Christmas ",
+                	artist:"Brandon Harris Walker"
+                }
+              ]
           },
           {
               names: 'Ann Sofi and Paul',
               playlistTitle: 'Boney M and friends Christmas playlist',
               playlist: 'PLq_oRfIJoxLfI9043reFSM7tD2Hbc6hi5',
-              cover:'/assets/images/covers/paulAnnsofi.jpg'
+              cover:'/assets/images/covers/paulAnnsofi.jpg',
+              tracks:[
+                {
+                	title:"Lonesome Christmas ",
+                	artist:"Lowell Fulson"
+                },
+                {
+                	title:"Have Yourself A Merry Little Christmas ",
+                	artist:" Frank Sinatra"
+                },
+                {
+                	title:"ROCKIN AROUND THE CHRISTMAS TREE ",
+                	artist:"BRENDA LEE"
+                },
+                {
+                	title:"It's the Most Wonderful Time of the Year",
+                	artist:"Andy Williams"
+                },
+                {
+                	title:"PLEASE COME HOME FOR CHRISTMAS",
+                	artist:" CHARLES BROWN "
+                },
+                 {
+                 	title:"Santa Baby",
+                	artist:"Eartha Kitt "
+                },
+                {
+                	title:"SLEIGH RIDE",
+                	artist:" THE BOSTON POPS ORCHESTRA"
+                },
+                {
+                	title:"Run Rudolph Run",
+                	artist:"Chuck Berry"
+                },
+                {
+                	title:"Let it Snow! ",
+                	artist:" Dean Martin"
+                },
+                {
+                	title:"Winter Wonderland",
+                	artist:"Louis Armstrong"
+                },
+                 {
+                 	title:"jingle bell rock",
+                	artist:"Bobby Helms"
+                },
+                {
+                	title:"White Christmas ",
+                	artist:"Bing Crosby"
+                },
+                 {
+                 	title:"I Saw Mommy Kissing Santa Claus",
+                	artist:"Jimmy Boyd "
+                },
+                {
+                	title:"Rudolph The Red Nosed Reindeer",
+                	artist:"Gene Autry"
+                },
+                {
+                	title:"The Christmas Song",
+                	artist:"Nat King Cole"
+                },
+                {
+                	title:"Happy Holidays ",
+                	artist:" Andy Williams"
+                },
+                {
+                	title:"God Rest Ye Merry Gentlemen playfishstuff1",
+                	artist:"Perry Como"
+                }
+                {
+                	title:"Blue Christmas",
+                	artist:"Elvis Presley"
+                }
+              ]
           }
       ]
     }
